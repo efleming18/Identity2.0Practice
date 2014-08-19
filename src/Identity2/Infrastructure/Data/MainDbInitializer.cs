f2b2fe1bf98a251bec8f01726410f3080800a002
Steve Smith <steve@kentsmiths.com>
2014-08-19 15:29:40 -0400

    Added a Db Initializer to seed some data

---
+++ src/Identity2/Infrastructure/Data/MainDbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using Identity2.Core.Model;
+
+namespace Identity2.Infrastructure.Data
+{
+    public class MainDbInitializer : DropCreateDatabaseIfModelChanges<MainDbContext>
+    {
+        protected override void Seed(MainDbContext context)
+        {
+            CreateInitialAccounts();
+            base.Seed(context);
+        }
+
+        private void CreateInitialAccounts()
+        {
+            var db = new MainDbContext();
+            db.Accounts.Add(new Account() {Name = "Acme"});
+            db.SaveChanges();
+        }
+    }
+}
