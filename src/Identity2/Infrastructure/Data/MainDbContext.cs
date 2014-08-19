bf0baf8e30ce628143a7b04da3acb4f5eae771a0
Steve Smith <steve@kentsmiths.com>
2014-08-19 15:23:44 -0400

    adding EF for users/accounts separate from identity db

---
+++ src/Identity2/Infrastructure/Data/MainDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using Identity2.Core.Model;
+
+namespace Identity2.Infrastructure.Data
+{
+    public class MainDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+        public MainDbContext() : base("MainConnection")
+        {
+        }
+    }
+}
